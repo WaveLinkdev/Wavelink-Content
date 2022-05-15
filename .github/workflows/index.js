778b9706a685df1e61563b4dfa61511b333af15a
WaveLink <jan.moser@email.cz>
2022-05-15 21:48:48 +0200

    asd

--- .github/workflows/index.js
+++ .github/workflows/index.js
@@ -1,5 +1,11 @@
 const fs = require('fs');
 
-fs.writeFile("test.json", JSON.stringify({message: "hello world"}));
+fs.writeFile("test.json", JSON.stringify({ message: "hello world" }), (err) => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log("successfully emitted index")
+    }
+});
 
 console.log("hello world");
